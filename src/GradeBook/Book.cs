5c4a58ba7d02420b93ade7a9d7dcb17348ccfc39
Seth NeJame <sethnejame@gmail.com>
2019-08-08 15:12:07 +0200

    Adding logic for computing average, high, low grades

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,13 +1,21 @@
 
+using System.Collections.Generic;
+
 namespace GradeBook
 {
   class Book
   {
+    public Book(string name)
+    {
+      grades = new List<double>();
+      this.name = name;
+    }
     public void AddGrade(double grade)
     {
-
+      grades.Add(grade);
     }
-    var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+    private List<double> grades;
+    private string name;
   }
 
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,20 +7,28 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
-      var result = 0.0;
 
-      var book = new Book();
+      var book = new Book("Seth's Grade Book");
       book.AddGrade(89.1);
- 
-      var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+      book.AddGrade(90.5);
+      book.AddGrade(101);
+      book.AddGrade(75);
 
-      foreach (double number in grades)
+      var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+      grades.Add(56.1);
+
+      var result = 0.0;
+      var highGrade = double.MinValue;
+      var lowGrade = double.MaxValue;
+      foreach (var number in grades)
       {
+        lowGrade = Math.Min(number, lowGrade);
+        highGrade = Math.Max(number, highGrade);
         result += number;
       }
-
       result /= grades.Count;
-
+      Console.WriteLine($"The lowest grade is {lowGrade:N1}.");
+      Console.WriteLine($"The highest grade is {highGrade:N1}.");
       Console.WriteLine($"The average grade is {result:N1}.");
 
     }
