ac5533c9f04f59dda86de900867e51c5e96a58fe
Seth NeJame <sethnejame@gmail.com>
2019-08-08 13:40:49 +0200

    adding arrays/lists for grades

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,17 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
+      var result = 0.0;
+      var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+      var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+      grades.Add(56.1);
+
+
+      foreach(double number in numbers) {
+        result += number;
+      }
+      Console.WriteLine(result);
+
       if (args.Length > 0)
       {
         Console.WriteLine($"Hello {args[0]}!");
