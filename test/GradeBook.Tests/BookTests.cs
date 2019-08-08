020c9d8fb3973e87dcf691a7120b84de6a6955f9
Seth NeJame <sethnejame@gmail.com>
2019-08-08 16:11:59 +0200

    Adding passing unit tests for GetStats

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -15,9 +15,9 @@ namespace GradeBook.Tests
 
           var result = book.GetStatistics();
 
-          Assert.Equal(85.6, result.Average);
-          Assert.Equal(90.5, result.High);
-          Assert.Equal(77.3, result.Low);
+          Assert.Equal(85.6, result.Average, 1);
+          Assert.Equal(90.5, result.High, 1);
+          Assert.Equal(77.3, result.Low, 1);
 
         }
     }
