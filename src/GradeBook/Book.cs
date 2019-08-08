49e904c1897ad30a39be4c454cb4bc4294065a1a
Seth NeJame <sethnejame@gmail.com>
2019-08-08 19:26:09 +0200

    Adding sln for project build

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -15,21 +15,21 @@ namespace GradeBook
       grades.Add(grade);
     }
 
-    public void GetStatistics()
+    public Statistics GetStatistics()
     {
-      var result = 0.0;
-      var highGrade = double.MinValue;
-      var lowGrade = double.MaxValue;
-      foreach (var number in grades)
+      var result = new Statistics();
+      result.Average = 0.0;
+      result.High = double.MinValue;
+      result.Low = double.MaxValue;
+      foreach (var grade in grades)
       {
-        lowGrade = Math.Min(number, lowGrade);
-        highGrade = Math.Max(number, highGrade);
-        result += number;
+        result.Low = Math.Min(grade, result.Low);
+        result.High = Math.Max(grade, result.High);
+        result.Average += grade;
       }
-      result /= grades.Count;
-      Console.WriteLine($"The lowest grade is {lowGrade:N1}.");
-      Console.WriteLine($"The highest grade is {highGrade:N1}.");
-      Console.WriteLine($"The average grade is {result:N1}.");
+      result.Average /= grades.Count;
+
+      return result;
     }
     private List<double> grades;
     private string name;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -13,8 +13,12 @@ namespace GradeBook
       book.AddGrade(90.5);
       book.AddGrade(101);
       book.AddGrade(75);
-      
-      book.ShowStatistics();      
+
+      var stats = book.GetStatistics();
+
+      Console.WriteLine($"The lowest grade is {stats.Low:N1}.");
+      Console.WriteLine($"The highest grade is {stats.High:N1}.");
+      Console.WriteLine($"The average grade is {stats.Average:N1}.");
 
     }
   }
