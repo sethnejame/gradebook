1bbf33bbe7411f745b40f0bb0f8451062dbd51d6
Seth NeJame <sethnejame@gmail.com>
2019-08-08 14:17:16 +0200

    Adding Book class

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,14 @@
+
+namespace GradeBook
+{
+  class Book
+  {
+    public void AddGrade(double grade)
+    {
+
+    }
+    var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+  }
+
+
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,9 +8,11 @@ namespace GradeBook
     static void Main(string[] args)
     {
       var result = 0.0;
-      var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+
+      var book = new Book();
+      book.AddGrade(89.1);
+ 
       var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-      grades.Add(56.1);
 
       foreach (double number in grades)
       {
@@ -21,14 +23,8 @@ namespace GradeBook
 
       Console.WriteLine($"The average grade is {result:N1}.");
 
-      if (args.Length > 0)
-      {
-        Console.WriteLine($"Hello {args[0]}!");
-      }
-      else
-      {
-        Console.WriteLine("Hello, stranger!");
-      }
     }
   }
+
+
 }
