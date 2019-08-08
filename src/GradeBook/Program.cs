077b8156d970ce6e35d8004beecb8c7a20b30653
Seth NeJame <sethnejame@gmail.com>
2019-08-08 15:29:30 +0200

    Logic now lives in Book class

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -13,23 +13,8 @@ namespace GradeBook
       book.AddGrade(90.5);
       book.AddGrade(101);
       book.AddGrade(75);
-
-      var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-      grades.Add(56.1);
-
-      var result = 0.0;
-      var highGrade = double.MinValue;
-      var lowGrade = double.MaxValue;
-      foreach (var number in grades)
-      {
-        lowGrade = Math.Min(number, lowGrade);
-        highGrade = Math.Max(number, highGrade);
-        result += number;
-      }
-      result /= grades.Count;
-      Console.WriteLine($"The lowest grade is {lowGrade:N1}.");
-      Console.WriteLine($"The highest grade is {highGrade:N1}.");
-      Console.WriteLine($"The average grade is {result:N1}.");
+      
+      book.ShowStatistics();      
 
     }
   }
