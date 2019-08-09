2089f08bfee6c806e1a35aa358b456b78199a9cb
Seth NeJame <sethnejame@gmail.com>
2019-08-09 07:04:49 +0200

    Adding unit test ~ change gradebook name

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,13 +6,37 @@ namespace GradeBook.Tests
   public class TypeTests
   {
     [Fact]
-    public void Test1()
+    public void CanChangeBookName()
+    {
+      var book = GetBook("Book1");
+      SetName(book, "Awesome Book!");
+
+      Assert.Equal("Awesome Book!", book.Name);
+
+    }
+
+    private void SetName(Book book, string name)
+    {
+      book.Name = name;
+    }
+
+    [Fact]
+    public void GetBookRetDiffObjects()
     {
       var book1 = GetBook("Book1");
       var book2 = GetBook("Book2");
 
       Assert.Equal("Book1", book1.Name);
       Assert.Equal("Book2", book2.Name);
+    }
+        [Fact]
+    public void TwoVarsCanRefSameObject()
+    {
+      var book1 = GetBook("Book1");
+      var book2 = book1;
+
+      Assert.Same(book1, book2);
+      Assert.True(Object.ReferenceEquals(book1, book2));
     }
 
     Book GetBook(string name)
