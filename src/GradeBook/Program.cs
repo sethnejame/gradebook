dcc555f018ccba705cc71527384ec3fb1f89aa11
Seth NeJame <sethnejame@gmail.com>
2019-08-10 09:22:29 +0200

    Adding user input for logging grades

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,17 +9,28 @@ namespace GradeBook
     {
 
       var book = new Book("Seth's Grade Book");
-      book.AddGrade(89.1);
-      book.AddGrade(90.5);
-      book.AddGrade(101);
-      book.AddGrade(75);
+
+      while(true)
+      {
+        Console.WriteLine("Enter a grade or 'q' to quit");
+        var input = Console.ReadLine();
+
+        if(input == "q")
+        {
+          Console.WriteLine("Exiting program . . .");
+          break;
+        }
+
+        var grade = double.Parse(input);
+        book.AddGrade(grade);
+      }
 
       var stats = book.GetStatistics();
 
       Console.WriteLine($"The lowest grade is {stats.Low:N1}.");
       Console.WriteLine($"The highest grade is {stats.High:N1}.");
       Console.WriteLine($"The average grade is {stats.Average:N1}.");
-      Console.WriteLine($"The letter grade is {stats.Letter}.");
+      Console.WriteLine($"The letter grade is {stats.Average:N1}.");
 
     }
   }
